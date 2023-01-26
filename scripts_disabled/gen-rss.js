2b1e9c6449d119d16103aa9c139603a57fe42fe6
穆鸣鸾 <101685830+mumingluan@users.noreply.github.com>
2023-01-26 10:29:35 +0000

    test

--- scripts_disabled/gen-rss.js
+++ scripts_disabled/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'mysitetitle',
+    site_url: 'https://scaramouche.live',
+    feed_url: 'https://scaramouche.live/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> Copyleft, MUML Group for Scaramouche.
       <style jsx>{`
         a {
           float: right;
