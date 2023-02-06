e41ff1fdf5290a9e5f2816063a53852a9c65a757
穆鸣鸾 <101685830+mumingluan@users.noreply.github.com>
2023-02-06 21:59:57 +0800

    rebuild theme

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> Copyleft, MUML Group for Scaramouche.
+      <time>{YEAR}</time>万叶 for Scaramouche.
       <style jsx>{`
         a {
           float: right;
